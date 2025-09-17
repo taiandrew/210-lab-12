3d5241c2d9d0425064010fa05862ca1c650e696e
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-09-16 22:07:07 -0700

    Fix array dtype; printArray()

--- tai-lab12.cpp
+++ tai-lab12.cpp
@@ -11,18 +11,29 @@ using namespace std;
 
 // Constants
 const int SIZE = 50;
+const string FILENAME = "/Users/andrewtai/Desktop/COMSC_210/projects/210-lab-12/roulette.txt";
 
 // Function prototypes
-void readFile(array<string, SIZE>& arr, string filename);
+void readFile(array<int, SIZE>& arr, string filename);
+void printArray(const array<int, SIZE>& arr);
 
 // Main
 int main() {
+
+    // Declare variables
+    array<int, SIZE> roulettes;
+
+    // Read file
+    readFile(roulettes, FILENAME);
+    cout << "Unsorted array:" << endl;
+    printArray(roulettes);
+
 
     return 0;
 }
 
 // Programmer functions
-void readFile(array<string, SIZE>& arr, string filename) {
+void readFile(array<int, SIZE>& arr, string filename) {
     // Reads contents of file into array
     // Args:
     // - arr: array to store file contents (pass by reference)
@@ -31,15 +42,27 @@ void readFile(array<string, SIZE>& arr, string filename) {
     // Open file
     ifstream fin(filename);
     if (!fin) {
-        cerr << "Input file opening failed.\n";
+        cerr << "Input file opening failed." << endl;
         return;
     }
 
     // Read file contents into array
     for (int i = 0; i < SIZE; ++i) {
         if (fin.eof()) break;
-        getline(fin, arr[i]);
+        fin >> arr[i];
     }
     fin.close();
+    return;
+}
+
+void printArray(const array<int, SIZE>& arr) {
+    // Prints contents of array
+    // Args:
+    // - arr: array to print (pass by reference)
+
+    for (const int& item : arr) {
+        cout << item << " ";
+    }
+    cout << endl;
     return;
 }
