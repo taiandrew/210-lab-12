17a1296412a9419b42c52ae3fa34846a62343692
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-09-16 21:58:41 -0700

    readFile()

--- tai-lab12.cpp
+++ tai-lab12.cpp
@@ -5,6 +5,46 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+// Constants
+const int SIZE = 50;
+
+// Function prototypes
+void readFile(array<string, SIZE>&, int&, string);
+
+// Main
+int main() {
+
+    return 0;
+}
+
+// Programmer functions
+void readFile(array<string, SIZE>& arr, int& size, string filename) {
+    // Reads contents of file into array
+    // Args:
+    // - arr: array to store file contents (pass by reference)
+    // - size: number of elements in array
+    // - filename: name of file to read from
+
+    // Open file
+    ifstream fin(filename);
+    if (!fin) {
+        cerr << "Input file opening failed.\n";
+        return;
+    }
+
+    // Read file contents into array
+    size = 0;
+    for (int i = 0; i < SIZE; ++i) {
+        if (getline(fin, arr[i])) {
+            ++size;
+        } else {
+            break;
+        }
+    }
+    fin.close();
+    return;
+}
